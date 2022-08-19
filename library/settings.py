53f203269152230ff951a63f4d960867faf52aa4
Roberto Rodriguez <rdrgzmndz.sm0101@gmail.com>
2022-08-19 18:13:17 -0500

    CORS fix

--- library/settings.py
+++ library/settings.py
@@ -113,12 +113,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'library.urls'
 
